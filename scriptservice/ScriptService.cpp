90df82e53cc0d361568bcc89666933be8f490a8c
ondravondra <ondrej.vondra@live.com>
2012-04-25 23:59:14 +0200

    cleaner design

--- scriptservice/ScriptService.cpp
+++ scriptservice/ScriptService.cpp
@@ -1,16 +1,9 @@
 #include "stdafx.h"
 #include "ScriptService.h"
 
-HRESULT CTestFactory::CreateScriptServiceInstance()
+HRESULT CTestFactory::InitScriptServiceInstance()
 {
   HRESULT hr;
-
-  hr = m_ScriptServiceInstance.CoCreateInstance(CLSID_ScriptServiceInstance);
-  if (FAILED(hr))
-  {
-    return hr;
-  }
-
   hr = m_ScriptServiceInstance->Init(L"myExtension", L"myDir");
   return hr;
 }
