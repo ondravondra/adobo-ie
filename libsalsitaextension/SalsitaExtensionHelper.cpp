81ea66d55f868dae2c20684f7b67cad0d0347270
ondravondra <ondrej.vondra@live.com>
2012-05-02 23:56:30 +0200

    added libsalsitaextension

---
+++ libsalsitaextension/SalsitaExtensionHelper.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "SalsitaExtensionHelper.h"
+#include <algorithm>
+
+void CSalsitaExtensionHelper::ResourcesDirNormalize(std::wstring &resourcesDir)
+{
+  if (resourcesDir.size() && resourcesDir.at(resourcesDir.size()-1) != L'\\')
+  {
+    resourcesDir.append(L"\\");
+  }
+}
+
+bool CSalsitaExtensionHelper::ResourcesDirReadFromRegistry(HKEY hKey, const wchar_t *subKey, const wchar_t *valueName, std::wstring &result)
+{
+  bool ret = false;
+  HKEY hkConfig;
+
+  result.erase();
+  if (RegOpenKeyExW(hKey, subKey, 0, KEY_READ, &hkConfig) == ERROR_SUCCESS)
+  {
+    DWORD valueType, valueLen = 0;
+    if (RegQueryValueExW(hkConfig, valueName, NULL, &valueType, NULL, &valueLen) == ERROR_SUCCESS && valueType == REG_SZ)
+    {
+      BYTE *buf = new BYTE[valueLen];
+      if (RegQueryValueExW(hkConfig, valueName, NULL, &valueType, buf, &valueLen) == ERROR_SUCCESS)
+      {
+        result.assign((const wchar_t *)buf);
+        ResourcesDirNormalize(result);
+        ret = true;
+      }
+      delete buf;
+    }
+
+    RegCloseKey(hkConfig);
+  }
+
+  return ret;
+}
+
+void CSalsitaExtensionHelper::ResourcesDirGetDebugPath(const wchar_t *preprocessorDefinedPath, std::wstring &result)
+{
+  result.assign(preprocessorDefinedPath);
+  std::replace(result.begin(), result.end(), L'/', L'\\');
+
+  std::wstring canonicalized;
+  canonicalized.resize(MAX_PATH+1);
+  PathCanonicalizeW((LPWSTR)result.c_str(), canonicalized.c_str());
+  result.assign(canonicalized.c_str());
+  ResourcesDirNormalize(result);
+}
--- staticlibs.cmake
+++ staticlibs.cmake
@@ -13,3 +13,13 @@ add_library(libcomhelper STATIC IMPORTED)
 set_property(TARGET libcomhelper PROPERTY IMPORTED_LOCATION_DEBUG "${libcomhelper_LIB_DIR_DEBUG}/${libcomhelper_LIB}")
 set_property(TARGET libcomhelper PROPERTY IMPORTED_LOCATION_RELEASE "${libcomhelper_LIB_DIR_RELEASE}/${libcomhelper_LIB}")
 
+set(libsalsitaextension_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/libsalsitaextension)
+set(libsalsitaextension_LIB "libsalsitaextension.lib")
+set(libsalsitaextension_LIB_DIR_DEBUG "${CMAKE_CURRENT_LIST_DIR}/_lib/${outdir_cfg_debug}")
+set(libsalsitaextension_LIB_DIR_RELEASE "${CMAKE_CURRENT_LIST_DIR}/_lib/${outdir_cfg_release}")
+
+add_library(libsalsitaextension STATIC IMPORTED)
+
+set_property(TARGET libsalsitaextension PROPERTY IMPORTED_LOCATION_DEBUG "${libsalsitaextension_LIB_DIR_DEBUG}/${libsalsitaextension_LIB}")
+set_property(TARGET libsalsitaextension PROPERTY IMPORTED_LOCATION_RELEASE "${libsalsitaextension_LIB_DIR_RELEASE}/${libsalsitaextension_LIB}")
+
